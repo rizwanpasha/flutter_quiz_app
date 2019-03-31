16bccb4ad7d22a4c455fc7f05c909e9667390647
rizwan pasha <rizwanpasha793@gmail.com>
2019-03-31 21:27:56 +0530

    crete quiz category selection page

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:fulutter_quiz_app/utils/quiz_service.dart';
+import 'utils/quiz_data.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const int PRIMARY_COLOR = 0xFF2C4058;
+const int SECONDARY_COLOR = 0xFFE78230;
+const int BACKGROUND_COLOR = 0xFF242A40;
 
 void main() {
   runApp(MyApp());
 
-  Map<String, String> quizCategories = {
-    "Animals": "animals.json",
-    "Animations": "animations.json",
-    "Computer Science": "computer_science.json",
-    "General Knowledge": "general_knowledge.json",
-    "Geography": "geography.json",
-    "History": "history.json",
-    "Science and Nature": "science_and_nature.json",
-    "Sports": "sports.json"
-  };
-
-  loadQuestions(quizCategories["Animations"]);
+  //loadQuestions(quizCategories["Animations"]);
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MainPage();
+  }
+}
+
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primaryColor: Color(PRIMARY_COLOR),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          backgroundColor: Color(BACKGROUND_COLOR),
+          appBar: AppBar(
+            title: Text(
+              'Quiz App',
+              style: TextStyle(fontSize: 26.0),
+            ),
+            centerTitle: true,
+          ),
+          body: QuizCategories()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class QuizCategories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return ListView.builder(
+      itemCount: QUIZ_DATA.length,
+      itemBuilder: (BuildContext context, int index) {
+        String categoryTitle = QUIZ_DATA[index]['title'];
+        String categoryIcon = QUIZ_DATA[index]['icon'];
+        int categoryColor = int.parse(QUIZ_DATA[index]['color']);
+
+        return Container(
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            child: Card(
+                clipBehavior: Clip.hardEdge,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                              onTap: () {},
+                              splashColor:
+                                  Color(categoryColor).withOpacity(0.4),
+                              child: Container(
+                                padding: EdgeInsets.all(10.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    categoryIcons(categoryIcon, categoryColor),
+                                    Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 5.0)),
+                                    Text(
+                                      categoryTitle,
+                                      style: TextStyle(
+                                          fontSize: 20.0,
+                                          color: Color(categoryColor)),
+                                    )
+                                  ],
+                                ),
+                              ))),
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text("TODO: Show stastics of $categoryTitle"),
+                      )
+                    ])));
+      },
     );
+  }
+}
+
+Icon categoryIcons(String iconName, int iconColor) {
+  const double fontSize = 30.0;
+  switch (iconName) {
+    case 'brain':
+      return Icon(
+        FontAwesomeIcons.brain,
+        size: fontSize,
+        color: Color(iconColor),
+      );
+      break;
+    case 'paw':
+      return Icon(FontAwesomeIcons.paw,
+          size: fontSize, color: Color(iconColor));
+      break;
+    case 'music':
+      return Icon(FontAwesomeIcons.music,
+          size: fontSize, color: Color(iconColor));
+      break;
+    case 'microscope':
+      return Icon(FontAwesomeIcons.microscope,
+          size: fontSize, color: Color(iconColor));
+      break;
+    case 'globeAfrica':
+      return Icon(FontAwesomeIcons.globeAfrica,
+          size: fontSize, color: Color(iconColor));
+      break;
+    case 'scroll':
+      return Icon(FontAwesomeIcons.scroll,
+          size: fontSize, color: Color(iconColor));
+      break;
+    case 'leaf':
+      return Icon(FontAwesomeIcons.leaf,
+          size: fontSize, color: Color(iconColor));
+      break;
+    case 'volleyballBall':
+      return Icon(FontAwesomeIcons.volleyballBall, color: Color(iconColor));
+      break;
   }
 }
---
+++ lib/utils/quiz_data.dart
@@ -0,0 +1,50 @@
+const List QUIZ_DATA = [
+  {
+    "title": "General Knowledge",
+    "file": "general_knowledge.json",
+    "icon": "brain",
+    "color": "0xFF3B3B98"
+  },
+  {
+    "title": "Animals",
+    "file": "animals.json",
+    "icon": "paw",
+    "color": "0xFFFC427B"
+  },
+  {
+    "title": "Entertainment",
+    "file": "entertainment.json",
+    "icon": "music",
+    "color": "0xFF1B9CFC"
+  },
+  {
+    "title": "Technology",
+    "file": "technology.json",
+    "icon": "microscope",
+    "color": "0xFFF97F51"
+  },
+  {
+    "title": "Geography",
+    "file": "geography.json",
+    "icon": "globeAfrica",
+    "color": "0xFF16a085"
+  },
+  {
+    "title": "History",
+    "file": "history.json",
+    "icon": "scroll",
+    "color": "0xFFf1c40f"
+  },
+  {
+    "title": "Science and Nature",
+    "file": "science_and_nature.json",
+    "icon": "leaf",
+    "color": "0xFF3c40c6"
+  },
+  {
+    "title": "Sports",
+    "file": "sports.json",
+    "icon": "volleyballBall",
+    "color": "0xFF1e272e"
+  }
+];
